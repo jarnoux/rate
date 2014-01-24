45698537727ad25a06153b2ced2bfc658fd1a7f3
Jacques Arnoux <arnoux@yahoo-inc.com>
2014-01-24 19:21:59 +0000

    added a hit method - having a measure that also affects the measure is just stinky

--- index.js
+++ index.js
@@ -4,33 +4,39 @@
  * @param {Number} millispan the time horizon for this Rate (in ms.)
  */
 var Rate = function (millispan) {
-		this.span = millispan;
-		this.events = [];
-	};
+        this.span = millispan;
+        this.events = [];
+    };
 
 /**
- * Retrieves a frequency. This method itself takes in the order of 0.06ms to run
- * with a time horizon of 1000ms.
- * @return  {Number} the frequency (in Hertz) at which this method
-*                        has been called in the past number of milliseconds
- *                        given in the constructor.
+ * Register a hit. This method takes in the order of 0.06ms to run
+ * with a time span of 1000ms.
+ */
+Rate.prototype.hit = function() {
+    var i = 0,
+        oldestEventIndex = 0,
+        now = Date.now(),
+        cutoffDate = now - this.span;
+
+    this.events.push(now);
+    for (; i < this.events.length; i++) {
+        if (this.events[i] > cutoffDate) {
+            oldestEventIndex = i;
+            break;
+        }
+    }
+    this.events = this.events.slice(oldestEventIndex);
+};
+
+/**
+ * Returns the hit frequency.
+ * @return  {Number} the frequency (in Hertz) at which Rate.prototype.hit
+ *                   has been called in the past number of milliseconds
+ *                   given in the constructor.
  */
 Rate.prototype.getFrequency = function () {
-	var i = 0,
-		oldestEventIndex = 0,
-		now = Date.now(),
-		cutoffDate = now - this.span;
 
-	this.events.push(now);
-	for (; i < this.events.length; i++) {
-		if (this.events[i] > cutoffDate) {
-			oldestEventIndex = i;
-			break;
-		}
-	}
-	this.events = this.events.slice(oldestEventIndex);
-
-	return this.events.length * 1000 / this.span;
+    return this.events.length * 1000 / this.span;
 };
 
 module.exports = Rate;
